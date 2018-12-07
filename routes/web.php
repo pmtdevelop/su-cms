2f093a26eda489d163eb368c6cc47de3e752ad22
Thanh Pham <thanhprodev@gmail.com>
2018-12-07 21:27:20 +0700

    add router

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('site/welcome');
+});
+
+Route::get('/admin', function () {
+    return view('admin/welcome');
 });
