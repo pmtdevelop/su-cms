3013024362b199bdccb298e648f38d54d46f9166
trinhcongduc <38092290+trinhcongduc@users.noreply.github.com>
2018-12-15 00:13:09 +0700

    update

--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="flex-center position-ref full-height">
+
         <div class="content">
             <div class="title m-b-md">
                 Tra cứu thông tin doanh nghiệp, văn bản pháp luật, tài liệu
@@ -9,3 +10,4 @@
         </div>
     </div>
 @endsection
+   
